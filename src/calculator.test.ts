723addc6ba953307b883381b5bd7af794ed7dc12
dreamnajababy <patnarin_s@kkumail.com>
2024-08-09 16:24:15 +0700

    feat(order-without-discount): add logic and test

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,5 +1,44 @@
+import Calculator, {
+  defaultBill,
+  Order,
+  itemPriceLookupTable,
+  Bill,
+} from "./calculator";
+
 describe("Calculator", () => {
-  it("should be return the correct answer", () => {
-    expect(1).toBe(1);
+  describe("Happy cases", () => {
+    test("When customer order red set without membership then get no discount", () => {
+      const order: Order = ["Red"];
+      const expectedBill = { ...defaultBill, Red: 1 };
+      const isMembership = false;
+      const expected: Bill = {
+        orderHistory: { ...defaultBill, Red: 1 },
+        totalPrice: itemPriceLookupTable["Red"],
+      };
+      const result = new Calculator().calculate(
+        order,
+        isMembership,
+        expectedBill
+      );
+      expect(result).toEqual(expected);
+    });
+    test.todo(
+      "When customer order red set with membership then get 10% discount"
+    );
+    test.todo(
+      "When customer order red set with orange set 2 item then customer get 5% discount"
+    );
+    test.todo(
+      "When customer order orange set 2 item then customer get 5% discount."
+    );
+    test.todo(
+      "When customer order orange set more than 2 item then customer get 5% discount"
+    );
+    test.todo(
+      "When customer order orange set more than 2 item with membership then get 10% and 5% discount"
+    );
+  });
+  describe("Edge cases", () => {
+    test.todo("When customer order no item then customer should get 0 price");
   });
 });
---
+++ src/calculator.ts
@@ -0,0 +1,43 @@
+export const defaultBill = {
+  Red: 0,
+  Green: 0,
+  Blue: 0,
+  Yellow: 0,
+  Pink: 0,
+  Purple: 0,
+  Orange: 0,
+} as const;
+type Price = number;
+export type Bill = {
+  orderHistory: Record<keyof typeof defaultBill, number>;
+  totalPrice: Price;
+};
+export const itemPriceLookupTable = {
+  Red: 50,
+  Green: 40,
+  Blue: 30,
+  Yellow: 50,
+  Pink: 80,
+  Purple: 90,
+  Orange: 120,
+} as const;
+
+export type Item = keyof typeof itemPriceLookupTable;
+export type Order = Item[];
+
+export default class Calculator {
+  public calculate(
+    items: Order,
+    isMembership: boolean,
+    bill: Record<string, number>
+  ): Bill {
+    const totalPrice = items.reduce(
+      (totalPrice, itemName) => totalPrice + itemPriceLookupTable[itemName],
+      0
+    );
+    return {
+      orderHistory: bill,
+      totalPrice,
+    };
+  }
+}
