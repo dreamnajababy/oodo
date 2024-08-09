1583f2444bb935e04300da035a7bf6dc46d3130f
dreamnajababy <patnarin_s@kkumail.com>
2024-08-09 17:18:49 +0700

    test(order-with-more-than-2-and-get-double-promotion): implement test and remove todo

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -31,6 +31,20 @@ describe("Calculator", () => {
       expect(result).toEqual(expected);
     });
     test("When customer order red set with orange set 2 item then customer get 5% discount", () => {
+      const order: Order = ["Orange", "Orange", "Red"];
+      const isMembership = false;
+      const expectedBill: CalculatedBill = {
+        orderHistory: { ...defaultBill, Orange: 2, Red: 1 },
+        totalPrice: 275.5,
+      };
+      const result = new Calculator().calculate(
+        order,
+        isMembership,
+        defaultBill
+      );
+      expect(result).toEqual(expectedBill);
+    });
+    test("When customer order orange set 2 item then customer get 5% discount.", () => {
       const order: Order = ["Orange", "Orange"];
       const isMembership = false;
       const expectedBill: CalculatedBill = {
@@ -44,12 +58,20 @@ describe("Calculator", () => {
       );
       expect(result).toEqual(expectedBill);
     });
-    test.todo(
-      "When customer order orange set 2 item then customer get 5% discount."
-    );
-    test.todo(
-      "When customer order orange set more than 2 item then customer get 5% discount"
-    );
+    test("When customer order pink set more than 2 item then customer get 5% discount", () => {
+      const order: Order = ["Pink", "Pink", "Pink"];
+      const isMembership = false;
+      const expectedBill: CalculatedBill = {
+        orderHistory: { ...defaultBill, Pink: 3 },
+        totalPrice: 228,
+      };
+      const result = new Calculator().calculate(
+        order,
+        isMembership,
+        defaultBill
+      );
+      expect(result).toEqual(expectedBill);
+    });
     test.todo(
       "When customer order orange set more than 2 item with membership then get 10% and 5% discount"
     );
