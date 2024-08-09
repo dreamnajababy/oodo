79d30a74954c703fdb914a14e4f54377b37ccc81
dreamnajababy <patnarin_s@kkumail.com>
2024-08-09 17:13:52 +0700

    refactor(order-with-double-item-promotion): group logic into function

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,3 +1,11 @@
+type Price = number;
+export type Bill = Record<keyof typeof itemPriceLookupTable, number>;
+export type CalculatedBill = {
+  orderHistory: Bill;
+  totalPrice: Price;
+};
+export type Item = keyof typeof itemPriceLookupTable;
+export type Order = Item[];
 export const itemPriceLookupTable = {
   Red: 50,
   Green: 40,
@@ -16,16 +24,6 @@ export const defaultBill: Bill = {
   Purple: 0,
   Orange: 0,
 };
-type Price = number;
-export type Bill = Record<keyof typeof itemPriceLookupTable, number>;
-export type CalculatedBill = {
-  orderHistory: Bill;
-  totalPrice: Price;
-};
-
-export type Item = keyof typeof itemPriceLookupTable;
-export type Order = Item[];
-
 export default class Calculator {
   public calculate(
     items: Order,
@@ -33,39 +31,43 @@ export default class Calculator {
     bill: Bill
   ): CalculatedBill {
     let totalPrice = items.reduce(
-      (accumulatePrice, name) => accumulatePrice + itemPriceLookupTable[name],
+      this.summarizeToPrice(itemPriceLookupTable),
       0
     );
-    const updatedBill = items.reduce(
-      (updatedBill, name) => {
-        if (name in updatedBill) {
-          updatedBill[name] += 1;
-        }
-        return updatedBill;
-      },
-      { ...bill }
-    );
-    const isDoubleItemPromotion = (updatedBill: Bill): boolean => {
-      if (updatedBill["Orange"] >= 2) {
-        return true;
-      }
-      if (updatedBill["Pink"] >= 2) {
-        return true;
-      }
-      if (updatedBill["Green"] >= 2) {
-        return true;
-      }
-      return false;
-    };
+    const updatedBill = items.reduce(this.incrementItemInBill, {
+      ...bill,
+    });
     if (isMembership) {
       totalPrice = totalPrice * 0.9;
     }
-    if (isDoubleItemPromotion(updatedBill)) {
+    if (this.isDoubleItemPromotion(updatedBill)) {
       totalPrice = totalPrice * 0.95;
     }
     return {
       orderHistory: updatedBill,
       totalPrice,
     };
+  }
+  private summarizeToPrice(itemPriceLookupTable: Record<Item, Price>) {
+    return (accumulatePrice: Price, name: Item) =>
+      accumulatePrice + itemPriceLookupTable[name];
+  }
+  private incrementItemInBill(updatedBill: Bill, name: Item) {
+    if (name in updatedBill) {
+      updatedBill[name] += 1;
+    }
+    return updatedBill;
+  }
+  private isDoubleItemPromotion(updatedBill: Bill): boolean {
+    if (updatedBill["Orange"] >= 2) {
+      return true;
+    }
+    if (updatedBill["Pink"] >= 2) {
+      return true;
+    }
+    if (updatedBill["Green"] >= 2) {
+      return true;
+    }
+    return false;
   }
 }
